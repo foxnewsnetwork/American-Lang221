2bb3a885be8856f3ca9aee89dc1843a25f412252
Thomas Chen <foxnewsnetwork@gmail.com>
2011-11-01 10:27:23 -0700

    added website part of the streaming service; no content yet, though

---
+++ ActiveStream/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveStream::Application.config.secret_token = 'c0c17511989b2ff6aac9c2e8832227f6903339030ad84bc16b118be33aa9af111af23db911526464daa1726a2e426803c1013dfd0713435b942d868f22816cf1'
