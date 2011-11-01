5da0005a95115f5d0ed910349c584bab2aff16a9
Thomas Chen <foxnewsnetwork@gmail.com>
2011-11-01 11:39:45 -0700

    using mysql db now

--- ActiveStream/config/initializers/secret_token.rb
+++ ActiveStream/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ActiveStream::Application.config.secret_token = 'c0c17511989b2ff6aac9c2e8832227f6903339030ad84bc16b118be33aa9af111af23db911526464daa1726a2e426803c1013dfd0713435b942d868f22816cf1'
+ActiveStream::Application.config.secret_token = '016ccca63b6fb92626065bd39c8edc5b9b802ae8c155108699f4596e3a689951cf1a1754b32503cba90eb03957fafbed8730701f556205404d1da91f66fba380'
