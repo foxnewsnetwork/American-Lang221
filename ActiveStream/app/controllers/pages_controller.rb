6327c5aadc0a8b520fbc636c7140184c9ea9c716
Thomas Chen <foxnewsnetwork@gmail.com>
2011-11-01 14:20:43 -0700

    put in text for splash page

--- ActiveStream/app/controllers/pages_controller.rb
+++ ActiveStream/app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+		@title = "ActiveStream"
   end
 
   def about
+		@title = "About A.S."
   end
 
   def contact
+		@title = "Contact us"
   end
 
 end
